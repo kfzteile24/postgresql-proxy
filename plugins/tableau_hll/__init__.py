d63b98cda77a98749f82dc39fa339c5addace094
Alexandru Pisarenco <pisarenco.a@gmail.com>
2019-02-25 16:22:14 +0100

    Improve tests, fix tableau regex

--- plugins/tableau_hll/__init__.py
+++ plugins/tableau_hll/__init__.py
@@ -4,7 +4,7 @@ import re
 # The field to replace
 field_pattern = re.compile('(?<=[^\w])count\(distinct (?:cast\()?("[^"]+")\.("[^"]+")(?: as text\))?\)', re.IGNORECASE)
 # Table name
-table_pattern = re.compile('from ([^\(\)]+)\s*\)? (?:AS )?("[^"]+")', re.IGNORECASE | re.DOTALL)
+table_pattern = re.compile('from ([^\(\)]+?)\s*\)? (?:AS )?("[^"]+")', re.IGNORECASE | re.DOTALL)
 
 def rewrite_query(query, context):
     original_table = ''
--- plugins/tableau_hll/test.py
+++ plugins/tableau_hll/test.py
@@ -9,13 +9,26 @@ def test_context():
     InstanceConfig = collections.namedtuple('InstanceConfig', 'redirect')
     Redirect = collections.namedtuple('Redirect', 'name host port')
     return {
-        'instance_config': InstanceConfig(redirect=Redirect(**config['redirect']))
+        'instance_config': InstanceConfig(redirect=Redirect(**config['redirect'])),
         'connect_params': config['connect_params']
     }
 
 
 def run():
-    query = 'SELECT COUNT(DISTINCT "crm_data_source"."Set of Customers") AS "ctd:Set of Customers:ok"\nFROM "crm_dim"."crm_data_source" "crm_data_source"\nHAVING (COUNT(1) > 0);'
-    out_query = 'SELECT  hll_cardinality(hll_union_agg("crm_data_source"."Set of Customers")) :: BIGINT  AS "ctd:Set of Customers:ok"\nFROM "crm_dim"."crm_data_source" "crm_data_source"\nHAVING (COUNT(1) > 0);'
+    queries = [
+        (
+            'SELECT COUNT(DISTINCT "crm_data_source"."Set of Customers") AS "ctd:Set of Customers:ok"\nFROM "crm_dim"."crm_data_source" "crm_data_source"\nHAVING (COUNT(1) > 0);',
+            'SELECT  hll_cardinality(hll_union_agg("crm_data_source"."Set of Customers")) :: BIGINT  AS "ctd:Set of Customers:ok"\nFROM "crm_dim"."crm_data_source" "crm_data_source"\nHAVING (COUNT(1) > 0);'
+        ),
+        (
+            'BEGIN;declare "SQL_CUR0x7fb46c01e3b0" cursor with hold for SELECT CAST("crm_data_source"."Campaign Name" AS TEXT) AS "Campaign Name",\n  COUNT(DISTINCT "crm_data_source"."Set of Unique Clicks") AS "usr:# Unique Customers (copy):ok"\nFROM "crm_dim"."crm_data_source" "crm_data_source"\nGROUP BY 1;fetch 2048 in "SQL_CUR0x7fb46c01e3b0"',
+            'BEGIN;declare "SQL_CUR0x7fb46c01e3b0" cursor with hold for SELECT CAST("crm_data_source"."Campaign Name" AS TEXT) AS "Campaign Name",\n   hll_cardinality(hll_union_agg("crm_data_source"."Set of Unique Clicks")) :: BIGINT  AS "usr:# Unique Customers (copy):ok"\nFROM "crm_dim"."crm_data_source" "crm_data_source"\nGROUP BY 1;fetch 2048 in "SQL_CUR0x7fb46c01e3b0"'
+        )
+    ]
     context = test_context()
-    assert hll.rewrite_query(query, context) == out_query
+    for src, dst in queries:
+        res = hll.rewrite_query(src, context)
+        try:
+            assert res == dst
+        except AssertionError:
+            print(f"Rewriting query:\n\n{src}\n\nExpecting:\n\n{dst}\n\nGot:\n\n{res}")
